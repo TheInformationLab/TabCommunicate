53274b23517b93e4914cfc56e76bd6b0e6c84757
craigbloodworth <craig.bloodworth@theinformationlab.co.uk>
2016-11-03 11:54:45 +0000

    Added nodalytics

--- appServer.js
+++ appServer.js
@@ -5,11 +5,11 @@ var http = require('http'),
 var bodyParser = require('body-parser')
 var cors = require('cors');
 var request = require("request");
+var nodalytics = require('nodalytics');
 
 app.set('trust proxy', 'loopback, linklocal, 172.30.52.0/24, 172.30.51.0/24');
 
-var ua = require('universal-analytics');
-var visitor = ua('UA-27427363-10', {https: true});
+app.use(nodalytics('UA-44126622-6'));
 
 var options = {};
 
@@ -30,7 +30,6 @@ app.use('/', function(req, res, next) {
   clientInfo.url = req.url;
   clientInfo.method = req.method;
   logStream.end(JSON.stringify(clientInfo)+"\n");
-  visitor.pageview(clientInfo.url, clientInfo.host, "Public").send();
   next();
 } , express.static(__dirname + '/'));
 
@@ -56,7 +55,6 @@ app.use('/api/q', function(req, res) {
   clientInfo.url = req.url;
   clientInfo.method = req.method;
   logStream.end(JSON.stringify(clientInfo)+"\n");
-  visitor.pageview(clientInfo.url, clientInfo.host, "Query API").send();
   request(options, function (error, response, body) {
     if (error) {
       var obj = {};
@@ -91,7 +89,6 @@ app.use('/api/tde', function(req, res) {
   clientInfo.url = req.url;
   clientInfo.method = req.method;
   logStream.end(JSON.stringify(clientInfo)+"\n");
-  visitor.pageview(clientInfo.url, clientInfo.host, "TDE API").send();
   var PythonShell = require('python-shell');
   getAllData(req, '', 0, function(csv) {
     //var response = 'data:application/csv;charset=utf-8,' + encodeURIComponent(csv);
@@ -148,7 +145,6 @@ app.use('/api/csv', function(req, res) {
   clientInfo.url = req.url;
   clientInfo.method = req.method;
   logStream.end(JSON.stringify(clientInfo)+"\n");
-  visitor.pageview(clientInfo.url, clientInfo.host, "CSV API").send();
   getAllData(req, '', 0, function(csv) {
     //var response = 'data:application/csv;charset=utf-8,' + encodeURIComponent(csv);
     //res.send(response);
@@ -181,7 +177,6 @@ app.use('/remote/tde', function(req, res) {
   clientInfo.url = req.url;
   clientInfo.method = req.method;
   logStream.end(JSON.stringify(clientInfo)+"\n");
-  visitor.pageview(clientInfo.url, clientInfo.host, "TDE Remote").send();
   var data = req.body;
   var fs = require('fs');
   var path = require('path');
